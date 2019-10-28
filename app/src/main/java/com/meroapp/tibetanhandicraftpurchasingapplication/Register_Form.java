855b317af67f8329e081b558bcec009527a31cd2
ashokbishwokarma96 <ashokbishwokarma96@gmail.com>
2019-10-28 19:05:20 +0545

    Register Form.

---
+++ app/src/main/java/com/meroapp/tibetanhandicraftpurchasingapplication/Register_Form.java
@@ -0,0 +1,14 @@
+package com.meroapp.tibetanhandicraftpurchasingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__form);
+    }
+}
